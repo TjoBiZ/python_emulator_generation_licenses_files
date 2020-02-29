6d1176d1de4e752fc6051e413557e1711941bbe2
joker <joker@tjo.biz>
2020-02-29 23:53:12 +0800

    1) Generator licenses files on python 2) PHP script query generator on python 2) Example generated license file

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$output = [];
+$return_var = '';
+//echo exec('./mm_licenses_c 123456 0001', $output, $return_var);
+echo exec('./gen.py F2CB56 0831', $output, $return_var);
+echo shell_exec('bash -c "bash command ./gen.py 121212 0022"');
+echo system('./gen.py'.escapeshellarg('001212 0023'),$output);
+echo exec('perl -e \'print `bash -c "bash command /Users/joker/Downloads/websites/python/php/./gen.py"`\'',$output, $return_var);
+echo exec('./gen.py', $output, $return_var);
+$getAnswer = exec('./gen.py', $output, $return_var);
+$getAnswer1 = exec('./gen.py 2342312', $output, $return_var);
+$getAnswer2 = exec('./gen.py 234231', $output, $return_var);
+$getAnswer3 = exec('./gen.py A2CB5S6 1001', $output, $return_var);
+$getAnswer4 = exec('./gen.py A2CB56 10S01', $output, $return_var);
+$getAnswer5 = exec('./gen.py A2CB56 1001', $output, $return_var);
+$getAnswer6 = exec('./gen.py A2CB56 1001 rstr rstr', $output, $return_var);
+
+print_r($output);
+var_dump($return_var);
+
+
+print phpinfo();
