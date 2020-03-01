f4954dcdf9e9dc481ff9ba5dffa46ba102206c64
joker <joker@tjo.biz>
2020-03-01 16:05:44 +0800

    1) Generator licenses files on python 2) PHP script query generator on python 2) Example generated license file

--- php/incrontab.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/sh
-cd /var/www/joker/data/www/marvelmind.tjo.biz/licensescron/
-/usr/bin/php getExitCodePython.php
--- php/inotify-monitor.sh
+++ php/inotify-monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 while inotifywait -e modify -e create -e delete /var/www/joker/data/www/marvelmind.tjo.biz/licensescron; do
-    /var/www/joker/data/www/marvelmind.tjo.biz/licensescron/./incrontab.sh
+    cd /var/www/joker/data/www/marvelmind.tjo.biz/licensescron/
+    /usr/bin/php getExitCodePython.php
 done
